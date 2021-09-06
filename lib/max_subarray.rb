e000752b132453f9ee9aa025de3a12a90ee9dd8d
Eric Hale <eric_hale@comcast.net>
2021-09-06 05:35:15 -0600

    2nd solution

--- lib/max_subarray.rb
+++ lib/max_subarray.rb
@@ -3,19 +3,14 @@ class MaxSubarray
         max_sum = array[0]
         current_sum = max_sum
         i = 1
-
-        return 0 if max_sum.nil?
+        
+        return 0 if max_sum.nil? || array.all?(&:negative?)
 
         until i == array.size
             current_sum = [current_sum + array[i], array[i]].max
             max_sum = [current_sum, max_sum].max
             i += 1
         end
-
-        if max_sum.negative?
-            return 0
-        else
-            max_sum
-        end
+        max_sum
     end
 end
